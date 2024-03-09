6531807ba39d0aa5525cd81ece5190c25b7280ac
guo chu <guochu604b@gmail.com>
2024-03-09 17:16:00 +0800

    change overlapcache

--- src/envs/overlap.jl
+++ src/envs/overlap.jl
@@ -17,8 +17,8 @@ end
 Base.length(x::InfiniteOverlapCache) = max(length(x.A), length(x.B))
 bra(x::InfiniteOverlapCache) = x.A
 ket(x::InfiniteOverlapCache) = x.B
-left_boundary(x::InfiniteOverlapCache) = x.left
-right_boundary(x::InfiniteOverlapCache) = x.right
+left_boundary(x::InfiniteOverlapCache) = x.left[1]
+right_boundary(x::InfiniteOverlapCache) = x.right[end]
 leading_eigenvalue(x::InfiniteOverlapCache) = x.η
 unitcell_size(x::InfiniteOverlapCache) = length(x)
 
@@ -30,17 +30,31 @@ function DMRG.environments(psiA::M, psiB::M) where {M <: Union{InfiniteMPS, Infi
 	right_eigenvalue, right_eigenvector = largest_eigenpair(x -> cell * x, vr)
 	(left_eigenvalue ≈ right_eigenvalue) || @warn "left and right dominate eigenvalues $(left_eigenvalue) and $(right_eigenvalue) mismatch"
 	@tensor trace = left_eigenvector[1,2] * right_eigenvector[2,1]
-	return InfiniteOverlapCache(psiA, psiB, lmul!(1/trace, left_eigenvector), right_eigenvector, left_eigenvalue)
+
+	L = length(Adata)
+	left = Vector{typeof(left_eigenvector)}(undef, L)
+	left[1] = lmul!(1/trace, left_eigenvector)
+	for i in 2:L
+		Adata_i, Bdata_i = get_common_data(psiA, psiB, i-1, i-1)
+		left[i] = left[i-1] * TransferMatrix(Adata_i, Bdata_i)
+	end
+	right = Vector{typeof(right_eigenvector)}(undef, L)
+	right[end] = right_eigenvector
+	for i in L-1:-1:1
+		Adata_i, Bdata_i = get_common_data(psiA, psiB, i+1, i+1)
+		right[i] = TransferMatrix(Adata_i, Bdata_i) * right[i+1]
+	end
+	return InfiniteOverlapCache(psiA, psiB, PeriodicArray(left), PeriodicArray(right), left_eigenvalue)
 end
 
-function DMRG.leftenv(x::InfiniteOverlapCache, i::Int)
-	pos = r_start(i, length(x))
-	Adata, Bdata = get_common_data(x.A, x.B, pos, i-1)
-	return left_boundary(x) * TransferMatrix(Adata, Bdata)
-end
-function DMRG.rightenv(x::InfiniteOverlapCache, i::Int)
-	pos = r_stop(i, length(x))
-	Adata, Bdata = get_common_data(x.A, x.B, i+1, pos)
-	return TransferMatrix(Adata, Bdata) * right_boundary(x)
-end
+DMRG.leftenv(x::InfiniteOverlapCache, i::Int) = x.left[i]
+DMRG.rightenv(x::InfiniteOverlapCache, i::Int) = x.right[i]
+
 DMRG.value(x::InfiniteOverlapCache, nperiod::Int=1) = leading_eigenvalue(x)^nperiod
+
+
+
+# for mixee-canonical form
+function DMRG.environments(psiA::M, psiB::M) where {M <: MixedCanonicalInfiniteMPS}
+	
+end
